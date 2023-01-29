d5d54c45fe22d2a97ad27a8d658be66d07f34341
Kishan Kr Sharma <skishan781@gmail.com>
2023-01-29 15:56:20 +0545

    Color scheme and design changed

--- DALL-E-2/HomeView.swift
+++ DALL-E-2/HomeView.swift
@@ -14,90 +14,108 @@ struct HomeView: View {
     @State var image: UIImage?
     @State var isLoading = false
     
+    
     var body: some View {
-        
-        NavigationView {
-            VStack {
-                Spacer()
+        VStack {
+            Text("AI Image Generator")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+            
+            Text("(Dall-E 2)")
+                .font(.system(size: 12))
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            if isLoading{
+                ProgressView("Generating an image")
+                    .tint(Color("icon"))
+                    .controlSize(.large)
+                    .font(.body)
+                    .padding()
+                    .background(Color("bg"))
+                    .cornerRadius(12)
+                    .shadow(radius: 5)
+                    .foregroundColor(Color("text"))
                 
-                if isLoading{
-                    ProgressView("Generating an image")
-                        .tint(Color("icon"))
-                        .controlSize(.large)
-                        .foregroundColor(.gray)
-                        .font(.body)
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(12)
-                        .shadow(radius: 5)
+            }else{
+                if let image = image{
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width-20, height: UIScreen.main.bounds.height/1.8)
+                        .padding(10)
+                        .background(Color("bg"))
+                    
+                    Button{
+                        print("Downloading...")
+                    }label: {
+                        Image(systemName: "square.and.arrow.down")
+                            .font(.system(size: 25))
+                            .aspectRatio( contentMode: .fit)
+                            .foregroundColor(Color("icon"))
+                            .frame(width: 25, height: 25)
+                            .padding(10)
+                        
+                    }
+                    .background(Color("textBox"))
+                    .cornerRadius(100)
+                    
+                    Text("Save to device")
+                        .foregroundColor(Color("text"))
+                    
                     
                 }else{
-                    if let image = image{
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: UIScreen.main.bounds.width-20, height: UIScreen.main.bounds.height/1.8)
-                            .padding(10)
-                            .background(Color.white)
-                        
-                    }else{
-                        Text(message)
-                    }
+                    Text(message).foregroundColor(Color("text"))
                 }
-                Spacer()
+            }
+            
+            Spacer()
+            
+            // Search box and send button
+            HStack(alignment: .center, spacing: 10){
+                TextField("Type here ...", text: $searchText)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 10)
+                    .background(Color("textBox"))
+                    .cornerRadius(100)
+                    .background(RoundedRectangle(cornerRadius: 100, style: .continuous)
+                        .stroke(.gray.opacity(0.6), lineWidth: 1.5))
                 
-                HStack(alignment: .center, spacing: 2){
-                    Button{
-                        print("regenerate")
-                    }label: {
-                        Image(systemName: "camera")
-                            .font(.system(size: 25))
-                            .foregroundColor(Color("icon"))
-                    }
-                    .padding(5)
-                    .cornerRadius(100)
-                    TextField("Type here ...", text: $searchText)
-                        .padding(.horizontal, 10)
-                        .padding(.vertical, 10)
-                        .background(Color("textBox"))
-                        .cornerRadius(100)
-                        .background(RoundedRectangle(cornerRadius: 100, style: .continuous)
-                            .stroke(.gray.opacity(0.6), lineWidth: 1.5))
-                    
-                    Button{
-                        
+                Button{
+                    let grab = searchText
+                    if !grab.trimmingCharacters(in: .whitespaces).isEmpty{
                         self.isLoading = true
-                        if !searchText.trimmingCharacters(in: .whitespaces).isEmpty{
-                            Task{
-                                let result = await viewModel.generateImage(prompt: searchText)
-                                
-                                self.isLoading = false
-                                
-                                if result == nil {
-                                    print("Failed to get an Image")
-                                    self.message = "Failed to generate an image!"
-                                }
-                                
-                                self.image = result
-                                
+                        Task{
+                            let result = await viewModel.generateImage(prompt: grab)
+                            
+                            self.isLoading = false
+                            
+                            if result == nil {
+                                print("Failed to get an Image")
+                                self.message = "Failed to generate an image!"
                             }
+                            self.image = result
                         }
-                    }label: {
-                        Image(systemName: "paperplane")
-                            .font(.system(size: 25))
-                            .foregroundColor(Color("icon"))
+                        self.searchText = ""
+                    }else{
+                        self.message = "Type something to generate an image."
                     }
-                    .padding(5)
-                    .cornerRadius(100)
+                }label: {
+                    Image(systemName: "paperplane")
+                        .font(.system(size: 25))
+                        .foregroundColor(Color("textBox"))
+                        .padding(5)
+                        .background(Color("icon"))
+                        .cornerRadius(100)
                 }
-            }
-            .onAppear{
-                viewModel.setup()
-            }
-            .navigationTitle("Dall-E 2")
-            .navigationBarTitleDisplayMode(.inline)
+            }.padding(.horizontal, 16)
         }
-        .navigationBarHidden(true)
+        .background(Color("bg"))
+        .onAppear{
+            viewModel.setup()
+        }
     }
     
 }
